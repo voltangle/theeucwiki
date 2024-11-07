297230c5dd2381196a486978364d39d1b975c084
GGorAA <ggoraa1029@gmail.com>
2024-11-07 20:16:01 +0100

    shubara: now runtime styles work properly letsgoooo

--- mediawiki/shubara/src/Hooks.php
+++ mediawiki/shubara/src/Hooks.php
@@ -1,11 +1,14 @@
 <?php
 namespace MediaWiki\Extension\Shubara;
 
-use MediaWiki\Context\RequestContext;
+use RequestContext;
 use MediaWiki\Hook\ParserFirstCallInitHook;
 use MediaWiki\Hook\RawPageViewBeforeOutputHook;
 use Parser;
 use PPFrame;
+use MediaWiki\Extension\CSS\Hooks as CSSExtHooks;
+use MediaWiki\Title\Title;
+use MediaWiki\Html\Html;
 
 class Hooks implements ParserFirstCallInitHook {
     public function onParserFirstCallInit( $parser ) {
@@ -21,10 +24,9 @@ class Hooks implements ParserFirstCallInitHook {
 
     // Render <navcard>
     public function renderTagNavCard( $input, array $args, Parser $parser, PPFrame $frame ) {
-        // $out = $parser->getOutput();
-        // $out->addInlineStyle( $cssString );
         $styleTag = $this->generateRandomString(26);
-        $output .= "<div class=\"mw-ext-shubara-navcard-$styletag\">";
+        $this->addStyles($parser, ".mw-ext-shubara-navcard-$styleTag { background-color: blue; }");
+        $output .= "<div class=\"mw-ext-shubara-navcard-$styleTag\">";
         $output .= htmlspecialchars($input);
         $output .= '<p>';
         $output .= implode(" ", $args);
@@ -43,6 +45,26 @@ class Hooks implements ParserFirstCallInitHook {
     
         return $randomString;
     }
-}
 
+    // made with help from the CSS extension
+    function addStyles(Parser $parser, string $css) {
+        $title = Title::newFromText($css);
+        $headItem = '<!-- Begin Extension:Shubara -->';
+        # Encode data URI and append link tag
+		$dataPrefix = 'data:text/css;charset=UTF-8;base64,';
+		$url = $dataPrefix . base64_encode( $css );
+
+		$headItem .= Html::linkedStyle( $url );
+        $headItem .= '<!-- End Extension:Shubara -->';
+		$parser->getOutput()->addHeadItem( $headItem );
+    }
+
+    function runWithExtension(string $ext, callback $callback) {
+        if (ExtensionRegistry::getInstance()->isLoaded($ext)) {
+            callback();
+        } else {
+            throw new BadFunctionCallException("Extension $ext is not loaded");
+        }
+    }
+}
 ?>
