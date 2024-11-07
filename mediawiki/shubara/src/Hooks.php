ccf3b2662907b31336c95409a95e99f655305b50
GGorAA <ggoraa1029@gmail.com>
2024-11-07 02:50:49 +0100

    shubara: very close to actually making the tag work

--- mediawiki/shubara/src/Hooks.php
+++ mediawiki/shubara/src/Hooks.php
@@ -1,17 +1,48 @@
 <?php
 namespace MediaWiki\Extension\Shubara;
 
-use MediaWiki\Extension\Shubara\Hooks\NavCards;
-use MediaWiki\Logger\LoggerFactory;
+use MediaWiki\Context\RequestContext;
+use MediaWiki\Hook\ParserFirstCallInitHook;
+use MediaWiki\Hook\RawPageViewBeforeOutputHook;
+use MediaWiki\Parser as Parser;
+use MediaWiki\Parser\PPFrame as PPFrame;
 
-$logger = LoggerFactory::getInstance( 'Shubara' );
-
-class Hooks implements ParserFirstCallInitHook, RawPageViewBeforeOutputHook {
+class Hooks implements ParserFirstCallInitHook {
     public function onParserFirstCallInit( $parser ) {
-        $parser->setHook( 'navcard', 'NavCards::renderTagNavCard' );
-        $parser->setHook( 'navcards', 'NavCards::renderTagNavCards' );
-        $logger->error('Set hooks successfully');
+        $parser->setHook( 'navcard', Hooks::renderTagNavCard(...) );
+        $parser->setHook( 'navcards', Hooks::renderTagNavCards(...) );
 		return true;
 	}
+
+    // render <navcards>
+    public function renderTagNavCards( $input, array $args, Parser $parser, PPFrame $frame ) {
+        return htmlspecialchars( $input );
+    }
+
+    // Render <navcard>
+    public function renderTagNavCard( $input, array $args, Parser $parser, PPFrame $frame ) {
+        // $out = $parser->getOutput();
+        // $out->addInlineStyle( $cssString );
+        $styleTag = generateRandomString(26);
+        $output .= "<div class=\"mw-ext-shubara-navcard-$styletag\">";
+        $output .= $input;
+        $output .= '<p>';
+        $output .= implode(" ", $args);
+        $output .= '</p></div>';
+        return htmlspecialchars( $output );
+    }
+
+    function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+    
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+    
+        return $randomString;
+    }
 }
+
 ?>
