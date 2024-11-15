688df113889bedf8d4cad661ae63b8dd7b98c6f9
GGorAA <ggoraa1029@gmail.com>
2024-11-15 21:48:06 +0100

    fix: HOLY FUCK NOW ITS FAST I finally fixed the very slow startup for the mediawiki container yall are allowed to send gifts btw

--- mediawiki/run-apache.sh
+++ mediawiki/run-apache.sh
@@ -109,7 +109,12 @@ run_script_if_needed () {
     fi
 }
 
-chown -R $WWW_USER:$WWW_GROUP $MW_VOLUME $MW_HOME
+# TODO: Check if this is needed for first startup
+# it turns out that this line right here is the reason why it took AGES for the mediawiki
+# container to start up, and removing it made it BLAZINGLY fast (please don't kill me).
+# But I suspect it's very much needed for first startup, so I have to check that ig
+
+# chown -R $WWW_USER:$WWW_GROUP $MW_VOLUME $MW_HOME
 
 cd $MW_HOME
 
