d8e5754f438633011b98063c046b84f307fe2b72
GGorAA <ggoraa1029@gmail.com>
2025-01-13 16:58:39 +0100

    elasticsearch: add script for initializing elasticsearch

---
+++ mediawiki/init_elastic.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Taken from https://www.mediawiki.org/wiki/MediaWiki-Docker/Extension/CirrusSearch#Modify_LocalSettings.php
+# This script has to be run once to initialize the ElasticSearch search index. Please run
+# from the docker compose directory with `task init-elastic`.
+
+# Configure the search index and populate it with content
+cd /var/www/html/w/
+until [ "`docker inspect -f {{.State.Health.Status}} elasticsearch`"=="healthy" ]; do
+    echo "Waiting for ElasticSearch to start...";
+    sleep 5;
+done;
+php extensions/CirrusSearch/maintenance/UpdateSearchIndexConfig.php
+php extensions/CirrusSearch/maintenance/ForceSearchIndex.php --skipLinks --indexOnSkip
+php extensions/CirrusSearch/maintenance/ForceSearchIndex.php --skipParse
+php maintenance/runJobs.php
+php extensions/CirrusSearch/maintenance/UpdateSuggesterIndex.php
