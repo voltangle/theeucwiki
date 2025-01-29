e6a907c910f2e2750e42152c86b11c1f650f3d44
GGorAA <ggoraa1029@gmail.com>
2025-01-29 02:01:56 +0100

    shubara: rework background/border color application for <imagechip>

--- shubara/src/Tags/Imagechip.php
+++ shubara/src/Tags/Imagechip.php
@@ -11,7 +11,8 @@ define("HEX_COLOR_REGEX", '/^#(?:[0-9a-fA-F]{3}){1,2}$/');
 /**
 * Render the imagechip tag
 */
-// TODO: finish this
+// NOTE: Ideally, this guy should be not a tag extension, but a parser hook, because it
+// takes wikitext input in multiple args, and a parser hook syntax will work better imo
 class Imagechip {
     /**
      * @param string $input What is supplied between the HTML tags. This gets evaluated
@@ -41,15 +42,12 @@ class Imagechip {
         $mode = $args['mode'] ?? 'col';
         // BUG: this is a GLARING SECURITY ISSUE you can just slap any CSS in there and
         // it will work. I will have to add some sanitization later on
-        $backgroundColor = $args['background-color'] ?? 'var(--color-surface-1)';
-        array_push($styles, "background-color: $backgroundColor;");
-        $borderColor;
-        if ($backgroundColor == 'var(--color-surface-1)') {
-            $borderColor = 'var(--color-surface-2)';
-        } else {
+        $backgroundColor = @$args['background-color'];
+        if ($backgroundColor != null) {
+            array_push($styles, "background-color: $backgroundColor;");
             $borderColor = Utils::adjustBrightness($backgroundColor, 0.2);
+            array_push($styles, "border: 4px solid $borderColor;");
         }
-        array_push($styles, "border: 4px solid $borderColor;");
     
         if (isset($args['flex']) && is_numeric(@$args['flex'])) {
             $flex = @$args['flex'];
