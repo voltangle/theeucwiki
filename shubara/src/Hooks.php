3355b735834e8dfc6194ae8f93e160525095b125
GGorAA <ggoraa1029@gmail.com>
2025-02-03 02:59:49 +0100

    shubara(feat): implement <projectstats>

--- shubara/src/Hooks.php
+++ shubara/src/Hooks.php
@@ -7,6 +7,7 @@ use MediaWiki\Extension\Shubara\Tags\Navcards;
 use MediaWiki\Extension\Shubara\Tags\Navcard;
 use MediaWiki\Extension\Shubara\Tags\Ulnav;
 use MediaWiki\Extension\Shubara\Tags\Imagechip;
+use MediaWiki\Extension\Shubara\Tags\Projectstats;
 
 class Hooks implements ParserFirstCallInitHook, BeforePageDisplayHook {
     public function onParserFirstCallInit( $parser ) {
@@ -14,6 +15,7 @@ class Hooks implements ParserFirstCallInitHook, BeforePageDisplayHook {
         $parser->setHook( 'navcard', Navcard::run(...) );
         $parser->setHook( 'ulnav', Ulnav::run(...) );
         $parser->setHook( 'imagechip', Imagechip::run(...) );
+        $parser->setHook( 'projectstats', Projectstats::run(...) );
 		return true;
 	}
 
---
+++ shubara/src/Tags/Projectstats.php
@@ -0,0 +1,40 @@
+<?php
+namespace MediaWiki\Extension\Shubara\Tags;
+
+use MediaWiki\Parser\Parser;
+use MediaWiki\Parser\PPFrame;
+use MediaWiki\Html\Html;
+use MediaWiki\Extension\Shubara\Utils;
+use MediaWiki\SiteStats\SiteStats;
+use MediaWiki\Output\OutputPage;
+use OOUI;
+
+/**
+* Render the sitestats tag
+*/
+// TODO: also add icons. I didn't do them from the start because they refuse to change color
+class Projectstats {
+    public static function run($input, array $args, Parser $parser, PPFrame $frame) {
+        $users = SiteStats::activeUsers();
+        $edits = SiteStats::edits();
+        // in Main, Help, and Category namespaces
+        $pages = SiteStats::pagesInNs(0)
+            + SiteStats::pagesInNs(12)
+            + SiteStats::pagesInNs(14);
+
+        $usersBox = self::makeBox('Users', $users);
+        $editsBox = self::makeBox('Edits', $edits);
+        $pagesBox = self::makeBox('Pages', $pages);
+        return Html::rawElement('div', ['class' => 'ext-shubara-projectstats'], $usersBox . $editsBox . $pagesBox);
+    }
+
+    static function makeBox(string $label, int $amount): string {
+        $amountHtml = Html::rawElement(
+            'p',
+            ['class' => 'ext-shubara-projectstats-amount'],
+            $amount);
+        $labelHtml = Html::rawElement('p', ['class' => 'ext-shubara-projectstats-label'], $label);
+
+        return Html::rawElement('div', [], $labelHtml . $amountHtml);
+    }
+}
